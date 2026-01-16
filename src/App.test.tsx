759c5cddd965cce3be2703b31fdc9aa13bdde52d
pandastackDev <sandy89bauchfic@htomail.com>
2026-01-16 15:42:23 +0000

    first commit for this sass ayout Velzon UI

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const { container } = render(<App />);
+  const linkElement = container.querySelector('a[href*="react"]');
+  expect(linkElement).toBeInTheDocument();
+});
